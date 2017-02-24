7c948fc5b05a931307c897678100fe20d9d3f65e
Kevin Amiranoff <kamiranoff@gmail.com>
2017-02-24 01:54:06 +0100

    adds redux logger

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import promise from 'redux-promise';
 import ReactDOM from 'react-dom';
+import  ApolloCient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import reducers from './reducers';
+
+import SongList from './Components/SongList';
+
+const client = new ApolloCient();
+const logger = createLogger({
+  collapsed:true,
+  diff:true,
+});
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer(),
+    reducers,
+  }),
+  {},
+  compose(
+    applyMiddleware(client.middleware(),thunk, promise, logger),
+    // If you are using the devToolsExtension, you can add it here also
+   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
+);
+
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider store={store} client={client}>
+      <SongList />
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
